9fb7511c7723ba74fe807a8f3269ad8e2a44792f
JAPJuan <108471144+JAPJuan@users.noreply.github.com>
2022-08-21 18:54:02 -0300

    pagina productos
    products.html hecha

---
+++ js/products.js
@@ -0,0 +1,41 @@
+
+let productsArray = [];
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+    document.getElementById("cat-name").innerHTML = "Verás aquí todos los productos de la categoría " + productsArray.catName
+
+    for(const product of array){ 
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ product.name + " - " + product.cost + " " + product.currency +`</h4> 
+                        <p> `+ product.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + product.soldCount + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            showProductsList(productsArray.products);
+
+        }
+    });
+});
