56c192a80d3b5ba49dd870dc7deff5fd317e5929
japjuan <108471144+japjuan@users.noreply.github.com>
2022-10-20 09:18:02 -0300

    Delete home.js

--- js/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-    for(const prodCard of document.getElementsByClassName("card mb-4 shadow-sm custom-card cursor-active")){
-        document.getElementById(prodCard.id).addEventListener("click", function() {
-            localStorage.setItem("catID", prodCard.id);
-            window.location = "products.html"
-        });
-    }
-});
